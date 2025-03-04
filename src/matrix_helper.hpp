4ad6cb60f666e0ab31945574e5186cb92e30fb51
Abir Haque <abir.haque.usa@gmail.com>
2025-03-04 17:42:10 -0600

    License information

--- src/matrix_helper.hpp
+++ src/matrix_helper.hpp
@@ -1,3 +1,43 @@
+/*
+ParFEMWARP is a parallel mesh warping program
+
+Copyright Notice:
+    Copyright 2025 Abir Haque
+
+License Notice:
+    This file is part of ParFEWMARP
+
+    ParFEMWARP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License Version 3 as published by
+    the Free Software Foundation.
+
+    ParFEMWARP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License Version 3 for more details.
+
+    You should have received a copy of the GNU Affero General Public License Version 3
+    along with ParFEMWARP in the file labeled LICENSE.txt.  If not, see https://www.gnu.org/licenses/agpl-3.0.txt
+
+
+Author: 
+    Abir Haque
+
+Date Last Updated: 
+    March 5th, 2025
+
+Notes: 
+    This software was developed by Abir Haque in collaboration with Dr. Suzanne M. Shontz at the University of Kansas (KU). 
+    This work was supported by the following:
+        HPC facilities operated by the Center for Research Computing at KU supported by NSF Grant OAC-2117449, 
+        REU Supplement to NSF Grant OAC-1808553, 
+        REU Supplement to NSF Grant CBET-2245153,
+        KU School of Engineering Undergraduate Research Fellows Program
+    If you wish to use this code in your own work, you must review the license at LICENSE.txt and cite the following paper:
+        Abir Haque, Suzanne Shontz. Parallelization of the Finite Element-based Mesh Warping Algorithm Using Hybrid Parallel Programming. SIAM International Meshing Roundtable Workshop (SIAM IMR 25), March 2025
+        Paper Link: https://internationalmeshingroundtable.com/assets/papers/2025/1018-compressed.pdf
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
